0ec73af7e04f6a6ed5fcd646ce198dad2b409a44
Daniel Gloger <daniel.gloger@cfturbo.com>
2024-11-07 16:22:41 +0100

    Simplify j2cli to work only with YAML, remove modules context and extras, hard code cp1252 encoding for output
    - Add Shebang for for POSIX systems
    - PEP-8: Every import in one line
    - First try to import yaml.FullLoader (PyYAML 5.1 supports FullLoader)
      else import yaml.SafeLoader
    - Use `open` instead of `io.open` (Python 3)
    - Jinja2TemplateRenderer contructor:
      - add parameter encoding (before this was hard-code utf-8)
      - remove `filters.env`
    - Function render_command, remove arguments:
      - `--version`
      - `--format`
      - `--import-env`
      - `"-" to use stdin`, this feature was removed for simplification
    
    - hard code cp1252 encoding for output
    
    - Remove the registering of filters and tests

--- j2cli/cli.py
+++ j2cli/cli.py
@@ -1,19 +1,28 @@
-import io, os, sys
+#! /usr/bin/env python
+
+"""j2cli main file"""
+
 import argparse
+import imp
+import inspect
+import os
+import sys
 
 import jinja2
-import jinja2.loaders
-from . import __version__
+import yaml
 
-import imp, inspect
 
-from .context import read_context_data, FORMATS
-from .extras import filters
-from .extras.customize import CustomizationModule
+# Loader
+try:
+    # PyYAML 5.1 supports FullLoader
+    Loader = yaml.FullLoader
+except AttributeError:
+    # Have to use SafeLoader for older versions
+    Loader = yaml.SafeLoader
 
 
 class FilePathLoader(jinja2.BaseLoader):
-    """ Custom Jinja2 template loader which just loads a single template file """
+    """Custom Jinja2 template loader which just loads a single template file"""
 
     def __init__(self, cwd, encoding='utf-8'):
         self.cwd = cwd
@@ -25,7 +34,7 @@ class FilePathLoader(jinja2.BaseLoader):
 
         # Read
         try:
-            with io.open(template, 'rt', encoding=self.encoding) as f:
+            with open(template, 'rt', encoding=self.encoding) as f:
                 contents = f.read()
         except IOError:
             raise jinja2.TemplateNotFound(template)
@@ -36,7 +45,7 @@ class FilePathLoader(jinja2.BaseLoader):
 
 
 class Jinja2TemplateRenderer(object):
-    """ Template renderer """
+    """Template renderer"""
 
     ENABLED_EXTENSIONS=(
         'jinja2.ext.i18n',
@@ -44,7 +53,10 @@ class Jinja2TemplateRenderer(object):
         'jinja2.ext.loopcontrols',
     )
 
-    def __init__(self, cwd, allow_undefined, j2_env_params):
+    def __init__(self, cwd, allow_undefined, encoding='utf-8', j2_env_params={}):
+        # Encoding
+        self.encoding = encoding
+
         # Custom env params
         j2_env_params.setdefault('keep_trailing_newline', True)
         j2_env_params.setdefault('undefined', jinja2.Undefined if allow_undefined else jinja2.StrictUndefined)
@@ -53,9 +65,6 @@ class Jinja2TemplateRenderer(object):
 
         # Environment
         self._env = jinja2.Environment(**j2_env_params)
-        self._env.globals.update(dict(
-            env=filters.env
-        ))
 
     def register_filters(self, filters):
         self._env.filters.update(filters)
@@ -85,10 +94,10 @@ class Jinja2TemplateRenderer(object):
         return self._env \
             .get_template(template_path) \
             .render(context) \
-            .encode('utf-8')
+            .encode(self.encoding)
 
 
-def render_command(cwd, environ, stdin, argv):
+def render_command(cwd, stdin, argv):
     """ Pure render command
     :param cwd: Current working directory (to search for the files)
     :type cwd: basestring
@@ -106,12 +115,6 @@ def render_command(cwd, environ, stdin, argv):
         description='Command-line interface to Jinja2 for templating in shell scripts.',
         epilog=''
     )
-    parser.add_argument('-v', '--version', action='version',
-                        version='j2cli {0}, Jinja2 {1}'.format(__version__, jinja2.__version__))
-
-    parser.add_argument('-f', '--format', default='?', help='Input data format', choices=['?'] + list(FORMATS.keys()))
-    parser.add_argument('-e', '--import-env', default=None, metavar='VAR', dest='import_env',
-                        help='Import environment variables as `var` variable. Use empty string to import into the top level')
     parser.add_argument('--filters', nargs='+', default=[], metavar='python-file', dest='filters',
                         help='Load custom Jinja2 filters from a Python file: all top-level functions are imported.')
     parser.add_argument('--tests', nargs='+', default=[], metavar='python-file', dest='tests',
@@ -121,127 +124,47 @@ def render_command(cwd, environ, stdin, argv):
     parser.add_argument('--undefined', action='store_true', dest='undefined', help='Allow undefined variables to be used in templates (no error will be raised)')
     parser.add_argument('-o', metavar='outfile', dest='output_file', help="Output to a file instead of stdout")
     parser.add_argument('template', help='Template file to process')
-    parser.add_argument('data', nargs='?', default=None, help='Input data file path; "-" to use stdin')
+    parser.add_argument('data', nargs='?', default=None, help='Input data file path')
     args = parser.parse_args(argv)
 
-    # Input: guess format
-    if args.format == '?':
-        if args.data is None or args.data == '-':
-            args.format = 'env'
-        else:
-            args.format = {
-                '.ini': 'ini',
-                '.json': 'json',
-                '.yml': 'yaml',
-                '.yaml': 'yaml',
-                '.env': 'env'
-            }[os.path.splitext(args.data)[1]]
-
-    # Input: data
-    # We always expect a file;
-    # unless the user wants 'env', and there's no input file provided.
-    if args.format == 'env':
-        # With the "env" format, if no dotenv filename is provided, we have two options:
-        # either the user wants to use the current environment, or he's feeding a dotenv file at stdin.
-        # Depending on whether we have data at stdin, we'll need to choose between the two.
-        #
-        # The problem is that in Linux, you can't reliably determine whether there is any data at stdin:
-        # some environments would open the descriptor even though they're not going to feed any data in.
-        # That's why many applications would ask you to explicitly specify a '-' when stdin should be used.
-        #
-        # And this is what we're going to do here as well.
-        # The script, however, would give the user a hint that they should use '-'
-        if args.data == '-':
-            input_data_f = stdin
-        elif args.data == None:
-            input_data_f = None
-        else:
-            input_data_f = open(args.data)
-    else:
-        input_data_f = stdin if args.data is None or args.data == '-' else open(args.data)
-
-    # Python 2: Encode environment variables as unicode
-    if sys.version_info[0] == 2 and args.format == 'env':
-        environ = dict((k.decode('utf-8'), v.decode('utf-8')) for k, v in environ.items())
-
-    # Customization
-    if args.customize is not None:
-        customize = CustomizationModule(
-            imp.load_source('customize-module', args.customize)
-        )
-    else:
-        customize = CustomizationModule(None)
-
-    # Read data
-    context = read_context_data(
-        args.format,
-        input_data_f,
-        environ,
-        args.import_env
-    )
-
-    context = customize.alter_context(context)
+    # Read YAML file and read condtext data
+    with open(args.data) as f:
+        context = yaml.load(f, Loader=Loader)
 
     # Renderer
-    renderer = Jinja2TemplateRenderer(cwd, args.undefined, j2_env_params=customize.j2_environment_params())
-    customize.j2_environment(renderer._env)
+    renderer = Jinja2TemplateRenderer(
+        cwd, args.undefined,
+        encoding='cp1252',
+        #j2_env_params={'newline_sequence': '\r\n'}
+        )
 
     # Filters, Tests
-    renderer.register_filters({
-        'docker_link': filters.docker_link,
-        'env': filters.env,
-    })
     for fname in args.filters:
         renderer.import_filters(fname)
     for fname in args.tests:
         renderer.import_tests(fname)
 
-    renderer.register_filters(customize.extra_filters())
-    renderer.register_tests(customize.extra_tests())
-
     # Render
-    try:
-        result = renderer.render(args.template, context)
-    except jinja2.exceptions.UndefinedError as e:
-        # When there's data at stdin, tell the user they should use '-'
-        try:
-            stdin_has_data = stdin is not None and not stdin.isatty()
-            if args.format == 'env' and args.data == None and stdin_has_data:
-                extra_info = (
-                    "\n\n"
-                    "If you're trying to pipe a .env file, please run me with a '-' as the data file name:\n"
-                    "$ {cmd} {argv} -".format(cmd=os.path.basename(sys.argv[0]), argv=' '.join(sys.argv[1:]))
-                )
-                e.args = (e.args[0] + extra_info,) + e.args[1:]
-        except:
-            # The above code is so optional that any, ANY, error, is ignored
-            pass
-
-        # Proceed
-        raise
+    result = renderer.render(args.template, context)
 
     # -o
     if args.output_file:
-        with io.open(args.output_file, 'wt', encoding='utf-8') as f:
+        with open(args.output_file, 'wt', encoding='utf-8') as f:
             f.write(result.decode('utf-8'))
-            f.close()
         return b''
 
     # Finish
     return result
 
 
-
-def main():
-    """ CLI Entry point """
+if __name__ == '__main__':
     try:
         output = render_command(
             os.getcwd(),
-            os.environ,
             sys.stdin,
             sys.argv[1:]
         )
     except SystemExit:
-        return 1
+        exit(1)
     outstream = getattr(sys.stdout, 'buffer', sys.stdout)
     outstream.write(output)
